e924138e9649a5eb0822dfe75825ad862427e6bd
Cpzty <Cristianpzty@gmail.com>
2018-04-20 12:59:38 -0600

    i dont know what happened

---
+++ Proyecto/compiler/DatabaseManager.py
@@ -0,0 +1,21 @@
+import os
+
+class DatabaseManager:
+        
+    def createDatabase(self, name):
+        directory = '../Bases/'+ name +'/'
+        try:
+            if not os.path.exists(directory):
+                os.makedirs(directory)
+                print ('Bases de datos' + name + 'creada')
+        except OSError:
+                print ('Error: Creating directory. ' +  directory)
+
+    def getDatabases(self):
+        bases = os.listdir('../Bases/')
+        print (bases)
+
+    def showTables(self, db_name):
+        tables = os.listdir('../Bases/' + db_name + '/')
+        print (tables)
+            
